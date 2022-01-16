fabc9f3725aabcdf276219302537ac72c4f4c5dd
Daniel Carmo <idd_dcarmo@uolinc.com>
2022-01-15 23:48:18 -0300

    Adiciona lib multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,11 @@
 import {Router} from 'express';
 import { createCategoryController } from '../modules/cars/UseCases/CreateCategoryUseCase';
 import { listCategoryController } from '../modules/cars/UseCases/ListCategoryUseCase';
+import multer from 'multer';
+
+const fileUpload = multer({
+    dest: './temp'
+});
 
 const categoriesRouter = Router();
 
@@ -10,6 +15,12 @@ categoriesRouter.post('/', (request, response) => {
 
 categoriesRouter.get('/', (_request, response) => {
     listCategoryController.handle(response);
+});
+
+categoriesRouter.post('/import', fileUpload.single('file'), (request, response) => {
+   const { file } = request;
+   console.log(file);
+   response.status(200).json(file);
 });
 
 export {categoriesRouter};
