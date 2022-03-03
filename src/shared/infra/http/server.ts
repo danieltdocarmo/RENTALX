0b43e25ddae8e8e3cb43961ce1a850131a75cfab
danieltdocarmo <danieldocarmo@Daniels-MacBook-Air.local>
2022-03-02 23:34:12 -0300

    Change server.ts

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,22 +1,4 @@
-import 'express-async-errors';
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import createConnection from '../typeorm'
-import swaggerFile from '../../../swaager.json';
-import { rentalxRoutes } from './routes';
-import err from './middlewares/err';
-
-const app = express();
-
-createConnection();
-
-app.use(express.json());
-
-app.use(rentalxRoutes);
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use(err);
+import {app} from './app';
 
 app.listen(3333, () => {
     console.log('Server is running on port 3333');
